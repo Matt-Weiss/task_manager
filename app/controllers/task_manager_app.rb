2c44179479c5502e465aae1e2b15416ce2ca1122
Matt Weiss <weiss.matt@outlook.com>
2019-03-06 13:27:42 -0700

    complete through task index route

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,11 @@ class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/' do
-    erb: dashboard
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = ["task1", "task2", "task3"]
+    erb :index
   end
 end
